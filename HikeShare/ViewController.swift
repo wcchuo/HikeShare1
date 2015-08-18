bf6909b8fcef01095b3e352678ba489459c4df22
wcchuo <wcchuo@yahoo.com>
2015-08-18 16:53:26 -0700

    Initial Commit

---
+++ HikeShare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HikeShare
+//
+//  Created by Wei Chung Chuo on 8/18/15.
+//  Copyright © 2015 Wei Chung Chuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
